f4510d921d456ce5a72fbbb0a1276e3b9c2bbf6f
Ryan Ban <45275840+RyanBan@users.noreply.github.com>
2021-08-18 17:27:39 -0400

    Created simple chat using websockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,38 @@
-hello;
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#msg");
+const frontSocket = new WebSocket(`ws://${window.location.host}`);
+
+function formatMessage(type, payload){
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+frontSocket.addEventListener("open",  () => {
+    console.log("Connected to Server BOOM");
+})
+
+frontSocket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+})
+
+frontSocket.addEventListener("close", ()=> {
+    console.log("Disconnected fom server");
+})
+
+function handleMsgSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    frontSocket.send(formatMessage("new_message", input.value));
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    frontSocket.send(formatMessage("nickname", input.value));
+}
+
+nickForm.addEventListener("submit", handleNickSubmit)
+messageForm.addEventListener("submit", handleMsgSubmit)
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,7 +8,35 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("start!");
 
-app.listen(3000, handleListen);
+//Handle http and wss together
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (backSocket) => {
+    sockets.push(backSocket);
+    backSocket["nickname"] = "Anonymous";
+    console.log("Connected to browser BOOM");
+    backSocket.on("close", ()=> console.log("Disconnected"));
+    backSocket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => {
+                    aSocket.send(`${backSocket.nickname}: ${message.payload}`)
+                });
+                break;
+            case "nickname":
+                backSocket["nickname"] = message.payload;
+                break;
+        }
+    });
+});
+
+//access to http server so I can create wss on top of the http server
+server.listen(3000, handleListen);
